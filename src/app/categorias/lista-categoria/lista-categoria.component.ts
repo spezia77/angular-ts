843c8fc71ba531fbcf36d7768708b2b604d20865
spezia77 <lhspezia777@gmail.com>
2024-10-30 22:14:12 -0300

    Conexão com back end

--- src/app/categorias/lista-categoria/lista-categoria.component.ts
+++ src/app/categorias/lista-categoria/lista-categoria.component.ts
@@ -4,9 +4,13 @@ import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
-import { NavbarComponent } from "../../navbar/navbar.component";
+import { CategoriaService } from '../../services/categoria.service';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { NavbarComponent } from '../../navbar/navbar.component';
 
-interface Categoria{
+interface Categoria {
   id: number,
   nome: string
 }
@@ -14,25 +18,130 @@ interface Categoria{
 @Component({
   selector: 'app-lista-categoria',
   standalone: true,
-  imports: [TableModule, ButtonModule, DialogModule, InputTextModule, FormsModule, NavbarComponent],
+  imports: [TableModule, ButtonModule, DialogModule, InputTextModule, FormsModule, ToastModule, ConfirmDialogModule, NavbarComponent],
+  providers: [ConfirmationService, MessageService],
   templateUrl: './lista-categoria.component.html',
   styleUrl: './lista-categoria.component.css'
 })
 export class ListaCategoriaComponent {
   nome: string = "";
   categorias!: Categoria[];
-  modalApresentada: boolean = false; 
+  modalApresentada: boolean = false;
+  idParaEditar!: number | undefined
 
-  ngOnInit(){
-    this.categorias = [
-      {
-        id: 1,
-        nome: "RPG"
-      }
-    ]
+  constructor(
+    private categoriaService: CategoriaService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+  ) { }
+
+  ngOnInit() {
+    this.carregarCategorias();
   }
 
-  abrirModal(){
+  private carregarCategorias() {
+    this.categoriaService.obterTodas().subscribe({
+      next: categorias => this.categorias = categorias,
+      error: erro => {
+        console.error(erro);
+        this.apresentarMensagemErro("Não foi possível carregar a lista de categorias");
+      }
+    });
+  }
+
+  abrirModal() {
     this.modalApresentada = true;
   }
+
+  salvar() {
+    if (!this.idParaEditar)
+      this.cadastrar();
+    else
+      this.editar();
+
+  }
+
+  private cadastrar() {
+    this.categoriaService.cadastrar(this.nome).subscribe({
+      next: () => {
+        this.apresentarMensagemSucesso("Cadastrado com sucesso");
+        this.modalApresentada = false;
+        this.nome = "";
+        this.carregarCategorias();
+      },
+      error: erro => {
+        console.error(erro);
+        this.apresentarMensagemErro("Erro ao cadastrar categoria");
+      }
+    });
+  }
+
+  private editar() {
+    this.categoriaService.editar(this.idParaEditar!, this.nome).subscribe({
+      next: () => {
+        this.apresentarMensagemSucesso("Editado com sucesso");
+        this.modalApresentada = false;
+        this.nome = "";
+        this.idParaEditar = undefined;
+        this.carregarCategorias();
+      },
+      error: erro => {
+        console.error(erro);
+        this.apresentarMensagemErro("Erro ao editar categoria");
+      }
+    });
+  }
+
+  confirmacaoExclusao(event: Event, categoria: Categoria) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: `Deseja realmente apagar a categoria '${categoria.nome}'?`,
+      header: 'Confirmação de exclusão',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: "p-button-danger p-button-text",
+      rejectButtonStyleClass: "p-button-text p-button-text",
+      acceptIcon: "none",
+      rejectIcon: "none",
+
+      accept: () => {
+        this.apagar(categoria.id)
+      }
+    });
+  }
+
+  apagar(id: number) {
+    this.categoriaService.apagar(id).subscribe({
+      next: () => {
+        this.apresentarMensagemSucesso("Apagado com sucesso")
+        this.carregarCategorias();
+      },
+      error: erro => {
+        console.error(erro);
+        this.apresentarMensagemErro("Erro ao apagar categoria")
+      }
+    })
+  }
+
+  apresentarMensagemSucesso(mensagem: string) {
+    this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: mensagem });
+  }
+  apresentarMensagemErro(mensagem: string) {
+    this.messageService.add({ severity: 'error', summary: 'Erro', detail: mensagem });
+  }
+
+  carregarModalParaEditar(id: number) {
+    this.categoriaService.obterPorId(id).subscribe({
+      next: categoria => {
+        this.nome = categoria.nome
+        this.abrirModal()
+        this.idParaEditar = id
+      },
+      error: erro => {
+        console.error(erro)
+        this.apresentarMensagemErro("Não foi possível carregar a categoria para editar");
+      }
+    })
+  }
 }
+
+
--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,27 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Categoria } from '../models/categorias';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
     obterTodas(): Observable<Categoria[]>{
-      let categorias = [
-        { nome: "Ação", id: 1 },
-        { nome: "Aventura", id: 2 },
-        { nome: "RPG", id: 3 },
-        { nome: "Estratégia", id: 4 },
-        { nome: "Simulação", id: 5 },
-        { nome: "Esporte", id: 6 },
-        { nome: "Tiro em Primeira Pessoa", id: 7 },
-        { nome: "Plataforma", id: 8 },
-        { nome: "Horror", id: 9 },
-        { nome: "Multiplayer Online", id: 10 }
-      ];
-      categorias.sort((a,b) => a.nome.localeCompare(b.nome));
-      return of(categorias);
+      return this.httpClient.get<Categoria[]>("http://127.0.0.1:8000/api/categorias/")
+    }
+
+    cadastrar(nome: string): Observable<any>{
+      let dados = {
+        nome
+      }
+      return this.httpClient.post("http://127.0.0.1:8000/api/categorias/", dados);
+    }
+
+    apagar(id: number): Observable<any>{
+      return this.httpClient.delete(`http://127.0.0.1:8000/api/categorias/${id}/`);
+    }
+
+    obterPorId(id: number): Observable<Categoria>{
+      return this.httpClient.get<Categoria>(`http://127.0.0.1:8000/api/categorias/${id}/`);
+    }
+
+
+    editar(id: number, nome: string): Observable<any>{
+      let dados = {
+        nome
+      }
+      return this.httpClient.put(`http://127.0.0.1:8000/api/categorias/${id}/`, dados);
     }
 }
